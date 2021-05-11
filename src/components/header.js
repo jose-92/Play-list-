ee8d961e4039bd9c7bbc8456c398a7594cc3b1d3
Jose Martinez <jemf9201@gmail.com>
2021-05-11 18:36:00 -0500

    agregando cambios en las card

--- src/components/header.js
+++ src/components/header.js
@@ -1,55 +1,44 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { BrowserRouter, Link } from "react-router-dom";
-import "./styles/CSS.css";
-import "bootstrap/dist/css/bootstrap.css";
-import Body from "./body";
-import Main from "./main";
-import Footer from "./footer";
 import ReactPlayer from "react-player";
+import Footer from "./footer";
+import "bootstrap/dist/css/bootstrap.css";
+import "./styles/CSS.css";
+import "./styles/body.css";
 
 const Header = () => {
   const [renderData, setRenderData] = useState([]);
   const [card, setCard] = useState([]);
-  // const KEY = "AIzaSyCRecMngvtKkNo-R0X_Y21P6hfitKsvGm4";
-  // const searchQuery = "motos";
-
-  // const handleSearch = async () => {
-  //   const res = await fetch(
-  //     `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&type=channel&q=${searchQuery}&safeSearch=none&key=${KEY}`
-  //   );
-  //   const data = await res.json();
-  //   console.log(data);
-  // &channelId=${channel_id}
-  // };
-
-  const handleSearch = () => {
-    const searchQuery = "motos  ";
-    const KEY = "AIzaSyCRecMngvtKkNo-R0X_Y21P6hfitKsvGm4";
-    // const CHANNEL_ID = "UCWKo4qLJ1sYd-rTQPihB7_Q";
-    axios
-      .get(
-        `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=2&type=channels=${searchQuery}1safeSearch=none&key=${KEY}`
-      )
-      .then((results) => {
-        const initialArray = results.data.items;
-        const firstResult = results.data.items.splice(0, 1);
-        console.log(firstResult);
-        setRenderData(firstResult);
-        const otherResults = initialArray;
-        setCard(otherResults);
-
-        setRenderData(results.data.items);
-        setCard(results.data.items);
-
-        console.log(results.data);
-        // console.log(card);
-      });
-  };
 
   useEffect(() => {
     handleSearch();
   }, []);
+
+  const handleSearch = () => {
+    const searchQuery = "motos honda";
+    const KEY = "AIzaSyCRecMngvtKkNo-R0X_Y21P6hfitKsvGm4";
+    // const CHANNEL_ID = "UCWKo4qLJ1sYd-rTQPihB7_Q";
+    axios
+      .get(
+        `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&type=video&q=${searchQuery}&safeSearch=none&key=${KEY}`
+      )
+      .then(({ data: { items } }) => {
+        const initialArray = items;
+        // console.log(firstResult);
+        const firstResult = items.splice(0, 1);
+        setRenderData(firstResult);
+        const otherResults = initialArray;
+        setCard(otherResults);
+
+        //   setRenderData(results.data.items);
+        //   setCard(results.data.items);
+
+        //   console.log(results.data);
+        //   // console.log(card);
+      });
+  };
+
   return (
     <BrowserRouter>
       <div className="container__pg">
@@ -83,7 +72,6 @@ const Header = () => {
         <br />
         <br />
 
-        {/* <Main /> */}
         <h4>Lista de canales de Youtube</h4>
         <br />
         <div>
@@ -103,14 +91,61 @@ const Header = () => {
                   href={`https://www.youtube.com/watch?v=` + x.id.videoId}
                 >
                   {x.snippet.description} <br />
-                  {x.snippet.title} <br />
+                  {/* {x.snippet.title} <br /> */}
                 </a>
               </div>
             ))}
-          <Body />
-          <Body />
         </div>
-        <Footer />
+        <div>
+          {card &&
+            card.map((x, id) => (
+              <div key={id} className="container__body">
+                <div>
+                  <ReactPlayer
+                    controls
+                    className="prf_img"
+                    width="400px"
+                    height="300px"
+                    url={`https://www.youtube.com/watch?v=` + x.id.videoId}
+                  />
+                  <a
+                    target="blank"
+                    href={`https://www.youtube.com/watch?v=` + x.id.videoId}
+                  >
+                    <p className="prf">{x.snippet.title}</p>
+                  </a>
+                  <a
+                    target="blank"
+                    href={`https://www.youtube.com/watch?v=` + x.id.videoId}
+                  >
+                    <p className="prf">{x.snippet.description}</p>
+                  </a>
+                </div>
+                <div>
+                  {/* <ReactPlayer
+                    controls
+                    className="prf_img"
+                    width="400px"
+                    height="300px"
+                    url={`https://www.youtube.com/watch?v=` + x.id.videoId}
+                  />
+                  <a
+                    target="blank"
+                    href={`https://www.youtube.com/watch?v=` + x.id.videoId}
+                  >
+                    <p className="prf">{x.snippet.title}</p>
+                  </a>
+                  <a
+                    target="blank"
+                    href={`https://www.youtube.com/watch?v=` + x.id.videoId}
+                  >
+                    <p className="prf">{x.snippet.description}</p>
+                  </a> */}
+                </div>
+              </div>
+            ))}
+          <Footer />
+        </div>
       </div>
     </BrowserRouter>
   );
